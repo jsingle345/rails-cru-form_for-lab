a30d9ba66c7b737d5a8cf952f9545b965df7a389
jsingle345 <jsingle345@gmail.com>
2020-10-13 08:07:28 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :songs do 
+    resources :genres
+    resources :artists
+  end
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
